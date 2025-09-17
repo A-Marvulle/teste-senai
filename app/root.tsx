fd2eeba246194da319af8164162247eca06d91b3
Alfredo Marvulle <alfredo.marvulle@gmail.com>
2025-09-17 20:48:06 -0300

    feat: adiciona configuracoes iniciais

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { NavLink } from "react-router";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +43,17 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+    <nav>
+      <NavLink to="/" >Home</NavLink>
+      <NavLink to="/sobre">Sobre</NavLink>
+      <NavLink to="/quiz">Quiz</NavLink>
+      <NavLink to="/contato">Contato</NavLink>
+    </nav>
+    <Outlet />
+    </>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"), 
+    route("sobre","routes/sobre.tsx"),
+    route("quiz","routes/quiz.tsx"),
+    route("contato","routes/contato.tsx"),
+    route("404","routes/404.tsx"),
+] satisfies RouteConfig;
